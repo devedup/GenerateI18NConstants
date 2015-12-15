ba19ade5a6e3ae6610197617479d8d88a7f8949a
Dan Love <dan@daniellove.net>
2015-12-15 17:09:33 +0000

    Demo outputs localized strings

--- GenerateI18NConstantsDemo/GenerateI18NConstantsDemo/ViewController.swift
+++ GenerateI18NConstantsDemo/GenerateI18NConstantsDemo/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
+		
+		print(Localized.AppTesting)
+		print(Localized.AppWelcomeMessage.with("Dan"))
 	}
 
 	override func didReceiveMemoryWarning() {
